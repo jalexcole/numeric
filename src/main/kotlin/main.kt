cadd56b51fdeb0038843ac56389931c322fed6ca
alex <jalexcole@gmail.com>
2021-01-13 18:42:41 -0600

    Adding helper functions.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,16 @@
-import java.util.*
-
+//import java.util.*
+import kotlin.system.measureNanoTime
+import kotlin.system.measureTimeMillis
+import kotlin.time.microseconds
 fun main(){
+  simpleBenchmark()
+}
 
+fun simpleBenchmark(){
+  val a = randomND(16000)
+  val b = randomND(16000)
+  val tic = measureTimeMillis {
+    val x = dotProduct(a, b)
+  }
+  println("Dot product takes ${tic} milli-seconds")
 }
